f8a144b4a76c0b2219e64137a26b5a7226e0cec8
andrew-oopey8 <andrew.oopey@gmail.com>
2023-07-05 17:28:40 +0300

    edit height

--- lib/web_view_page.dart
+++ lib/web_view_page.dart
@@ -37,7 +37,7 @@ class _WebViewAppState extends State<WebViewApp> {
       child: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        padding: const EdgeInsets.only(top: 8),
+        padding: const EdgeInsets.only(top: 16),
         child: WebViewStack(
           controller: controller,
         ),
